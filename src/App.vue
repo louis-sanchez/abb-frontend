40051bdbd68dbed92e94c666060fe3b2ec367f73
lsanchez <l.sanchez@outlook.com>
2021-05-03 13:44:51 +0200

    SPIKE!
    Get a basic frontend up and running with websockets

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h2>ABB Frontend</h2>
+    <button v-on:click="sendMessage('hello')">Send Message</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      connection: null,
+    };
+  },
+  methods: {
+    sendMessage: function (message) {
+      console.log("Hello");
+      console.log(this.connection);
+      this.connection.send(message);
+    },
+  },
+  created: function () {
+    console.log("Starting connection to WebSocket Server");
+    this.connection = new WebSocket("ws://localhost:3000");
+
+    this.connection.onopen = function (event) {
+      console.log(event);
+      console.log("Successfully connected to the echo websocket server...");
+    };
+
+    this.connection.onmessage = function (event) {
+      console.log(event);
+    };
+  },
+};
 </script>
 
 <style>
